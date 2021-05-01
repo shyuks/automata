c934b96057e453a52dadad29029faaa5f899451b
Sean Hong <s.hong35@gmail.com>
2021-05-01 13:34:07 -0700

    Create new empty 2D array, so we don't overwrite original 2D array.

--- automata.js
+++ automata.js
@@ -16,3 +16,32 @@
 
 // cell is 3
 // new cell is 0
+
+const computeGenerations = (matrix, count) => {
+  // create a new empty 2D array so we don't overwrite original 2D array;
+  const newMatrix = [];
+  const matrixLength = matrix.length;
+  for (let x = 0; x < matrixLength; x++) {
+    newMatrix[x] = [];
+    for (let y = 0; y < matrixLength; y++) {
+      newMatrix[x][y] = 0;
+    }
+  }
+
+  console.log(newMatrix);
+}
+
+const testOne = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 2, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 2, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+computeGenerations(testOne, 1);
